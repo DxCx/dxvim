a917f923af77e433043d7a05ecfb36cbadffdd5f
Hagai Cohen <hagai.co@gmail.com>
2023-09-08 04:44:04 +0300

    Plugins update
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- packages/neovim/config/lsp/plugins.nix
+++ packages/neovim/config/lsp/plugins.nix
@@ -9,22 +9,22 @@ let
 
   tree-sitter-playground = vimUtils.buildVimPluginFrom2Nix {
     pname = "tree-sitter-playground";
-    version = "unstable-2023-04-15";
+    version = "unstable-2023-08-26";
     src = fetchFromGitHub {
       owner = "nvim-treesitter";
       repo = "playground";
-      rev = "2b81a018a49f8e476341dfcb228b7b808baba68b";
-      sha256 = "1b7h4sih8dc55w12f0v5knk9cxfpy0iffhbvmg0g84if55ar616v";
+      rev = "429f3e76cbb1c59fe000b690f7a5bea617b890c0";
+      sha256 = "1flqiycr7dm4cyp2gpy3dmkk8xcdk4268kgmp5qz43qf7fi8m7iy";
     };
   };
   refactoring-nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "refactoring-nvim";
-    version = "unstable-2023-07-26";
+    version = "unstable-2023-08-31";
     src = fetchFromGitHub {
       owner = "ThePrimeagen";
       repo = "refactoring.nvim";
-      rev = "5359e74291164fcaeaaecdea9ba753ad54eb53d0";
-      sha256 = "1jskfd63n3r1slwhbv1qv239nd0mp3q7h4r9fardkc4xf7nsy1jb";
+      rev = "2ec9bc0fb5f3c8c6a0f776f0159dd2a3b1663554";
+      sha256 = "038cczxj9ba3axb3aw5r2dsp5anzacnwnnp61i1pk7kk8l3wg2ck";
     };
   };
 in
--- packages/neovim/config/maximizer/plugins.nix
+++ packages/neovim/config/maximizer/plugins.nix
@@ -5,12 +5,12 @@
 let
   maximizer-nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "maximizer.nvim";
-    version = "unstable-2023-07-07";
+    version = "unstable-2023-08-13";
     src = fetchFromGitHub {
       owner = "0x00-ketsu";
       repo = "maximizer.nvim";
-      rev = "839b7cf3a1fa46c020fc874d5224274836d54691";
-      sha256 = "0bp2fv1j6rcfzr5n8g1g1vw9mwn4xlnkhnsx13qla5hy62qkrldp";
+      rev = "4779ea731b4babaae7a5a406d455bd7fd1971f85";
+      sha256 = "0jiky21z0k7lqcq1hwri0rw1gamw1shm7cpg16y5aj1vwi8barrl";
     };
   };
 in
