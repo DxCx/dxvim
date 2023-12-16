317468c8e36611480f55054a7df5baf421cbe6b7
Hagai Cohen <hagai.cohen@nextsilicon.com>
2023-12-16 20:14:11 +0000

    copilot: Fix key bindings in panel mode
    Signed-off-by: Hagai Cohen <hagai.cohen@nextsilicon.com>

--- packages/neovim/config/copilot/init.lua
+++ packages/neovim/config/copilot/init.lua
@@ -5,7 +5,21 @@ local which_key = require("which-key")
 
 copilot.setup({
 	suggestion = { enabled = false },
-	panel = { enabled = false },
+	panel = {
+		enabled = true,
+		auto_refresh = true,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>"
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4
+		},
+	},
 	filetypes = {
 		gitcommit = true,
 		markdown  = true,
