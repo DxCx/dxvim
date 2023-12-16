83f82d7b3d39c6f6fc4698fe869e805481276c35
Hagai Cohen <hagai.cohen@nextsilicon.com>
2023-12-16 21:32:03 +0000

    copilot: Add CopilotChat plugin (doesn't seem to work - TODO)
    Signed-off-by: Hagai Cohen <hagai.cohen@nextsilicon.com>

---
+++ packages/neovim/config/copilot/extra-packages.nix
@@ -0,0 +1,7 @@
+{ python311Packages, ... }:
+with python311Packages; [
+  python-dotenv
+  requests
+  pynvim
+  prompt-toolkit
+]
--- packages/neovim/config/copilot/plugins.nix
+++ packages/neovim/config/copilot/plugins.nix
@@ -1,7 +1,14 @@
-{ vimPlugins
-, ...
-}:
-with vimPlugins; [
-  copilot-lua
-  copilot-cmp
-]
+{ vimPlugins, vimUtils, fetchFromGitHub, ... }:
+let
+  copilot-chat-nvim = vimUtils.buildVimPlugin {
+    pname = "copilot-chat-nvim";
+    version = "unstable-2023-12-15";
+    src = fetchFromGitHub {
+      owner = "gptlang";
+      repo = "CopilotChat.nvim";
+      rev = "a45d44fee7e6d0bf36a95742a4073aa2ea2acfb6";
+      sha256 = "1y5bjby695r3m06m62h4ndfmpkpi8c09smpaygsq632c7ah8xkb2";
+    };
+  };
+
+in with vimPlugins; [ copilot-chat-nvim copilot-lua copilot-cmp ]
