938342ab6c4a9e0804149e183d47bca3497c5d60
Hagai Cohen <hagai.co@gmail.com>
2023-12-24 08:23:50 +0200

    hop: Update to latest hopefully problem is fixed, if not revert

--- packages/neovim/config/movement/plugins.nix
+++ packages/neovim/config/movement/plugins.nix
@@ -6,13 +6,13 @@ let
   # Latest version result in bugs =(
   # See https://github.com/phaazon/hop.nvim/issues/345
   hop-nvim-patched = vimPlugins.hop-nvim.overrideAttrs (old: {
-    version = "reverted_2023-05-17";
+    version = "unstable-2022-11-08";
     src = fetchFromGitHub {
       #owner = "phaazon";
       owner = "aznhe21";
       repo = "hop.nvim";
-      rev = "2d3085e40154813b034bce1ad034f0d323c0c581";
-      sha256 = "CTGyxMySz1amkz/87YQ7y1YfaWMNLK7UN9IACc4ahHk=";
+      rev = "a7454d16a762a2f50f235e68f35b30b0f20f3a35";
+      sha256 = "0v4h0axz66ijwn4xms2j1rrsm7jc0mk30kngkgfbcnv3bf4xmr95";
     };
   });
 in
