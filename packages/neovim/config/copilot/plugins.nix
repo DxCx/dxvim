226a41932a360cb2b8846f28fca27173f0e8ffd8
Hagai Cohen <hagai.co@gmail.com>
2024-02-18 08:26:32 +0200

    flake: Update deps
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- packages/neovim/config/copilot/plugins.nix
+++ packages/neovim/config/copilot/plugins.nix
@@ -2,12 +2,12 @@
 let
   copilot-chat-nvim = vimUtils.buildVimPlugin {
     pname = "copilot-chat-nvim";
-    version = "unstable-2024-02-16";
+    version = "unstable-2024-02-18";
     src = fetchFromGitHub {
       owner = "CopilotC-Nvim";
       repo = "CopilotChat.nvim";
-      rev = "2ae6b8c9b8c9f226d0e019fa27e9b6a2263ff220";
-      sha256 = "1wmnrd3rqw18a38ln3dp0yf9qfnbn263y358vb5nhnqd4f3v92k9";
+      rev = "e46fa23fe7c43a29c849fb9b6a1d565d2e0b83f1";
+      sha256 = "1sh0j2gb6zxp81wfk3sqp2pjai02x29ymg237rsay2an2a9jq8rj";
     };
   };
 
