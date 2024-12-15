3384f36112a1953306024bbd5eb57b08f15803c9
Hagai Cohen <hagai.co@gmail.com>
2024-12-15 21:11:40 +0200

    flake: Update dependancies
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- packages/neovim/config/copilot/plugins.nix
+++ packages/neovim/config/copilot/plugins.nix
@@ -7,12 +7,12 @@
 let
   copilot-chat-nvim = vimUtils.buildVimPlugin {
     pname = "copilot-chat-nvim";
-    version = "unstable-2024-12-05";
+    version = "unstable-2024-12-14";
     src = fetchFromGitHub {
       owner = "CopilotC-Nvim";
       repo = "CopilotChat.nvim";
-      rev = "1fe19d1fdbf9edcda8bad9b7b2d5e11aa95c1672";
-      sha256 = "1fz4mls0ixqjgc5112740nd5k3rw7f48in6q4b9l1xkif2kjks90";
+      rev = "2ebe591cff06018e265263e71e1dbc4c5aa8281e";
+      sha256 = "1lw43yhpbi3kl6j5lmz027yhndqjp95ms123dx9lxyqpfa6zkwr0";
     };
   };
 in
