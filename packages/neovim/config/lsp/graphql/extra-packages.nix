0db540762a8bb6ba27b009850487e84ab1222b89
Hagai Cohen <hagai.co@gmail.com>
2025-02-27 12:58:31 +0200

    packages: Disable graphql lang service
    As node packages were removed =\
    
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- packages/neovim/config/lsp/graphql/extra-packages.nix
+++ packages/neovim/config/lsp/graphql/extra-packages.nix
@@ -1,4 +1,22 @@
-{ pkgs, nodePackages, ... }:
-
-with pkgs;
-[ nodePackages.graphql-language-service-cli ]
+# TODO: Re-enable later?
+#{
+#  fetchFromGitHub,
+#  buildNpmPackage,
+#  ...
+#}:
+#let
+#  graphql-language-service-cli = buildNpmPackage rec {
+#    name = "graphql-language-service-cli";
+#    version = "3.5.0";
+#    src = fetchFromGitHub {
+#      owner = "graphql";
+#      repo = "graphiql";
+#      rev = "${name}@${version}";
+#      # replace with lib.fakeHash to extract newer hash.
+#      hash = "sha256-NJTggaMNMjOP5oN+gHxFTwEdNipPNzTFfA6f975HDgM=";
+#    };
+#    sourceRoot = "${src.name}/packages/graphql-language-service-cli/";
+#  };
+#in
+#[ graphql-language-service-cli ]
+_: [ ]
